264b596026c9720d512baea8e358e7509a23e39d
Heozilla <vuthaitai2000@gmail.com>
2026-05-19 00:07:33 +0700

    blabla

--- ex02/ex02_08.py
+++ ex02/ex02_08.py
@@ -5,3 +5,10 @@ def chia_het_cho_5(so_nhi_phan):
     else:
         return False
 chuoi_so_nhi_phan = input("Nhập chuỗi số nhị phân (phân tách bởi dấu phẩy): ")
+so_nhi_phan_list = chuoi_so_nhi_phan.split(',')
+so_chia_het_cho_5 = [so for so in so_nhi_phan_list if chia_het_cho_5 (so)]
+if len(so_chia_het_cho_5) > 0:
+    ket_qua = ','.join(so_chia_het_cho_5)
+    print("Các số nhị phân chia hết cho 5 là:", ket_qua)
+else:
+    print("Không có số nhị phân nào chết hết cho 5 trong chuỗi đã nhập.")
